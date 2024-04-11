badbfc86daf47eb280c37bae1ab6d0299efa6342
Semjon Kravtsenko <semjon.00@gmail.com>
2024-04-11 13:07:22 +0300

    Sub code for HPC v4

--- Solution/submission.py
+++ Solution/submission.py
@@ -19,7 +19,7 @@ def encode_mask(mask: np.ndarray):
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Using {} device".format(device))
 dataset = LethalDataset('../data/test', 96, 100)
-loader = DataLoader(dataset, batch_size=1, shuffle=True)  # !=1 is not supported
+loader = DataLoader(dataset, batch_size=4, shuffle=True)
 
 submission_df = pd.DataFrame(columns=['ImageID', 'RleMonsters', 'RleLoot'])
 model = torch.load(sys.argv[1]).to(device)
@@ -27,17 +27,17 @@ with torch.inference_mode():
     i = 0
     for batch in loader:
         i += 1
-        if i % 20 == 0:
+        if i % 10 == 0:
             print('.', end='')
-            if i % 1000 == 0:
+            if i % 100 == 0:
                 print('|')
         name, img = batch[0], batch[1].to(device)
         seg, det = model(img)
         en_prediction, lt_prediction = pred_to_masks(seg, det)
-
-        en_encoded = encode_mask(en_prediction[0].detach().cpu())[1]
-        lt_encoded = encode_mask(lt_prediction[0].detach().cpu())[1]
-        submission_df.loc[len(submission_df.index)] = [name, en_encoded, lt_encoded]
+        for i in range(len(name)):
+            en_encoded = encode_mask(en_prediction[i].detach().cpu())
+            lt_encoded = encode_mask(lt_prediction[i].detach().cpu())
+            submission_df.loc[len(submission_df.index)] = [name[i], en_encoded, lt_encoded]
 
 
 submission = pd.DataFrame(
